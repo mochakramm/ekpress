b7ed6f5d54474e28bd763674984d8bf45f5adef0
Akram Azkia <molenakram8@gmail.com>
2024-08-20 16:44:47 +0700

    commit ketiga

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! ganteng')
+app.get('/index', (req, res) => {
+  res.sendFile('./index.html', {root:__dirname})
 })
 app.get('/about', (req, res) => {
-  res.send('ini adalah halaman about')
+  res.sendFile('./about.html', {root:__dirname})
 })
 app.get('/contact', (req, res) => {
-  res.send('ini adalah halaman contact')
+  res.sendFile('./contact.html', {root:__dirname})
+})
+
+
+app.get('/product/:id', (req, res) => {
+  res.send(`product id : ${req.params.id} <br> category id : ${req.query.category}` )
 })
 
 app.use('/',(req,res) => {
-  res.send('test')
+  res.status(404)
+  res.send('test (error) ')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 
 
 
