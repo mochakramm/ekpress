2900c962ea3ba8d1841059f965fd7eaeb034d55c
Akram Azkia <molenakram8@gmail.com>
2024-08-22 11:21:35 +0700

    commit ke empat

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/index', (req, res) => {
-  res.sendFile('./index.html', {root:__dirname})
+//gunakan ejs
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+ // res.sendFile('./index.html', {root:__dirname})
+    res.render('index')
 })
 app.get('/about', (req, res) => {
-  res.sendFile('./about.html', {root:__dirname})
+    res.render('about')
 })
 app.get('/contact', (req, res) => {
-  res.sendFile('./contact.html', {root:__dirname})
+    res.render('contact')
 })
 
 
@@ -19,8 +24,7 @@ app.get('/product/:id', (req, res) => {
 })
 
 app.use('/',(req,res) => {
-  res.status(404)
-  res.send('test (error) ')
+  res.render('index')
 })
 
 
@@ -38,51 +42,3 @@ app.listen(port, () => {
 
 
 
-// const http = require('http');
-// const fs = require('fs');
-// const port =3000
-
-
-// const renderHTML = (path, res) => {
-//     fs.readFile(path, (e, data) => {
-//         if (e) {
-//             res.writeHead(404)
-//             res.write('error: file not found')
-
-//         } else {
-//             res.write(data)
-//         }
-//         res.end()
-//     })
-// }
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, {
-//       'Content-Type': 'text/html',
-//     });
-
-//     const url = req.url;
-
-//     switch (url) {
-//         case '/about':
-//             renderHTML ('./about.html', res)
-//             break
-//         case '/contact':
-//             renderHTML('./contact.html', res)
-//             break
-//         default:
-//             renderHTML('./index.html',res)
-//             break
-//     }
-
-//     // if (url === '/about') {
-//     //   renderHTML('./about.html', res)
-//     //  } else if (url === '/contact') {
-//     //   renderHTML('./contact.html', res)
-//     // } else {
-//     //  renderHTML('./index.html', res)
-//     // }
-//   })
-//   .listen(port, () => {
-//     console.log('server is listening on port 3000...');
-//   });
